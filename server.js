348980e7133792ae09a623a9b887da1da432ba76
Willy0la <132810663+Willy0la@users.noreply.github.com>
2025-02-09 20:35:37 +0100

    delete route

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import newTask from "./controller/newTask.js";
 import getallTask from "./controller/getTask.js";
 import getbyId from "./controller/getTaskid.js";
 import modifyTask from "./controller/update.js";
+import deleteTask from "./controller/deleteTask.js";
 const app = express();
 
 connectDB()
@@ -18,7 +19,7 @@ route.post("/newTask", newTask);
 route.get("/tasks", getallTask);
 route.get("/tasks/:taskId", getbyId);
 route.put("/update/:taskId", modifyTask);
-
+route.delete("/delete/:taskId", deleteTask)
 
 app.listen(2025, () => {
   console.log("Server is listening on port 2025");
